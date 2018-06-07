dfbad944cc24575cce19ae89352ed99117614930
Nicolas de Chevigné <nico2che@gmail.com>
2018-06-07 17:35:25 +0200

    [CORE] remove not found page

--- src/router.js
+++ src/router.js
@@ -3,17 +3,15 @@ import { Switch, BrowserRouter, Redirect, Route } from 'react-router-dom';
 
 import Vote from './containers/Vote';
 import Score from './containers/Score';
-import NotFound from './containers/not-found';
 
 export default class Router extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Redirect path="/" exact to="/vote" />
           <Route path="/vote" component={Vote}/>
           <Route path="/score" component={Score}/>
-          <Route component={NotFound}/>
+          <Redirect exact to="/vote" />
         </Switch>
       </BrowserRouter>
     );
