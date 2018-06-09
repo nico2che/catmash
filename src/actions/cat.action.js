78c2775188ec73a7615a4ce684410df11726e5c2
Nicolas de Chevigné <nico2che@gmail.com>
2018-06-09 10:59:25 +0200

    [VOTE] add cat actions

---
+++ src/actions/cat.action.js
@@ -0,0 +1,23 @@
+export const GET_CAT = 'GET_CAT';
+export const GET_CAT_SUCCESS = 'GET_CAT_SUCCESS';
+export const GET_CAT_FAILURE = 'GET_CAT_FAILURE';
+
+export function getCat() {
+  return {
+    type: GET_CAT
+  }
+}
+
+export function getCatSuccess({ cats }) {
+  return {
+    type: GET_CAT_SUCCESS,
+    cats
+  }
+}
+
+export function getCatFailure({ error }) {
+  return {
+    type: GET_CAT_FAILURE,
+    error
+  }
+}
