2965cea300b110213ba22fb37c9c0cfaef8ff1bf
Nicolas de Chevigné <nico2che@gmail.com>
2018-06-09 13:13:51 +0200

    [CORE] intialize redux & middlewares

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import createSagaMiddleware from 'redux-saga';
+
+import reducers from './reducers';
+import sagas from './sagas';
 import Router from './router';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware),
+);
+
+sagaMiddleware.run(sagas);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
