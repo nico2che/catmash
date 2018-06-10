1d83e2725d3ec721c9cf9129574abd60caf0fd74
Nicolas de Chevigné <nico2che@gmail.com>
2018-06-10 20:32:41 +0200

    [VOTE] add score actions

---
+++ src/actions/score.action.js
@@ -0,0 +1,49 @@
+export const GET_SCORE = 'GET_SCORE';
+export const GET_SCORE_SUCCESS = 'GET_SCORE_SUCCESS';
+export const GET_SCORE_FAILURE = 'GET_SCORE_FAILURE';
+export const UPDATE_SCORE = 'UPDATE_SCORE';
+export const UPDATE_SCORE_SUCCESS = 'UPDATE_SCORE_SUCCESS';
+export const UPDATE_SCORE_FAILURE = 'UPDATE_SCORE_FAILURE';
+
+export function getScore() {
+  return {
+    type: GET_SCORE,
+  }
+}
+
+export function getScoreSuccess({ cats }) {
+  return {
+    type: GET_SCORE_SUCCESS,
+    cats,
+  }
+}
+
+export function getScoreFailure({ error }) {
+  return {
+    type: GET_SCORE_FAILURE,
+    error,
+  }
+}
+
+export function updateScore({ catWinner, catLoser }) {
+  return {
+    type: UPDATE_SCORE,
+    catWinner,
+    catLoser,
+  }
+}
+
+export function updateScoreSuccess({ catWinner, catLoser }) {
+  return {
+    type: UPDATE_SCORE_SUCCESS,
+    catWinner,
+    catLoser,
+  }
+}
+
+export function updateScoreFailure({ error }) {
+  return {
+    type: UPDATE_SCORE_FAILURE,
+    error,
+  }
+}
